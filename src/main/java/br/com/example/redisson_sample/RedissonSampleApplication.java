6d757c7e0875d3c422c7b9d5c52e8665d7cf5daa
Paulo Sérgio <pauloqueiroz16@gmail.com>
2025-02-09 19:30:44 -0300

    First commit: basic structure of the project

---
+++ src/main/java/br/com/example/redisson_sample/RedissonSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.redisson_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedissonSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/redisson_sample/RedissonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.redisson_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
